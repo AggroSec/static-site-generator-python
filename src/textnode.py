80f5a283e0d27974d58a9f3dc6114ef99489383c
AggroSec <btmaides1019@gmail.com>
2026-02-18 11:45:58 -0500

    fixed __eq__ in TextNode class to correctly compare 2 TextNodes

--- src/textnode.py
+++ src/textnode.py
@@ -16,7 +16,13 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return self == other
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text == other.text and
+            self.text_type == other.text_type and
+            self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
