e3abeacab38c8829ca1c7b3683e9672ae2f0b107
AggroSec <btmaides1019@gmail.com>
2026-02-24 09:51:28 -0500

    corrected links so that CSS and links work

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from markdown_html_conversion import *
 from htmlnode import *
 from copier import copier
 import os
+import sys
 
 def extract_title(markdown):
     split = markdown.splitlines()
@@ -14,7 +15,7 @@ def extract_title(markdown):
         raise Exception("no title found")
     return title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     file = open(from_path, "r")
     content = file.read()
@@ -27,23 +28,26 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_str)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     new_file = open(dest_path, "w")
     new_file.write(template)
     new_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, item)
         dst_path = os.path.join(dest_dir_path, item)
         if not os.path.isfile(src_path):
-            generate_pages_recursive(src_path, template_path, dst_path)
+            generate_pages_recursive(src_path, template_path, dst_path, basepath)
         else:
-            generate_page(src_path, template_path, dst_path.replace(".md", ".html"))
+            generate_page(src_path, template_path, dst_path.replace(".md", ".html"), basepath)
 
 def main():
-    copier("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1]
+    copier("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == "__main__":
